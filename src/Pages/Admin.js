915ec544a9f05b1a8ba1156cb3fa74c1804388d7
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-07-21 13:15:04 +0500

    first commit

---
+++ src/Pages/Admin.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import ShowProducts from "./ShowProducts";
+import AddProducts from "./AddProducts";
+import Sidebar from './Sidebar';
+import { useState, useEffect } from 'react';
+import Users from './Users';
+
+
+const Admin = () => {
+
+    const [data, setdata] = useState([])
+    const getapi = async () => {
+        const getdata = await fetch('https://backend-puce-zeta.vercel.app/addproducts')
+        const result = await getdata.json()
+        setdata(result)
+
+    }
+
+    useEffect(() => {
+        getapi()
+    }, [])
+
+
+    return (
+        <>
+            <div className='flex flex-col lg:flex-row'>
+                <Sidebar ></Sidebar>
+                <Routes>
+                    <Route path="/addproducts" element={<AddProducts getapi={getapi} data={data}></AddProducts>}></Route>
+                    <Route path="/showproducts" element={<ShowProducts getapi={getapi} data={data}></ShowProducts>}></Route>
+                    <Route path="/users" element={<Users getapi={getapi} data={data}></Users>}></Route>
+                </Routes>
+            </div>
+        </>
+    )
+}
+
+export default Admin
---
+++ src/Pages/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import svg from '../Admin Panel Assets/Product_Cart.svg'
+import remove from '../Admin Panel Assets/Product_list_icon.svg'
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className=' lg:w-[240px]   flex lg:flex-col items-center bg-white justify-around lg:justify-normal lg:h-[100dvh] ' >
+                <div className='  w-[170px] flex lg:w-[79%]  bg-slate-100 items-center p-2 text-blue-600 my-12 cursor-pointer'>
+                    <Link to={'/addproducts'} className='flex justify-around' >  <img src={svg} alt="" />
+                        <span>Add Products</span></Link>
+                </div>
+                <div className=' w-[170px] lg:w-[70%]  bg-slate-100 items-center p-2 text-blue-600 cursor-pointer'>
+                    <Link to={'showproducts'} className='flex justify-around'>    <img src={remove} alt="" />
+                        <span>Product List</span></Link>
+                </div>
+                <div className=' w-[170px] my-10 lg:w-[70%]  bg-slate-100 items-center p-2 text-blue-600 cursor-pointer'>
+                    <Link to={'/users '} className='flex justify-around'>    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBOOmyZaeLf1DWtIXaPTuz4hCuWntSyrBowA&usqp=CAU' alt="" className='h-[30px] object-contain' />
+                        <span>All Users</span></Link>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Sidebar
